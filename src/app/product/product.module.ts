87b7e2cbec18b9890423446dc0c7522c93f4a1d1
vareversat <valentin.reversat22@gmail.com>
2019-04-11 22:07:00 +0200

    new: masonry

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
+import { NgMasonryGridModule } from 'ng-masonry-grid';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgMasonryGridModule
   ],
   declarations: [ListComponent],
   exports: [ListComponent]
